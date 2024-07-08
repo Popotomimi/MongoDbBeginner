34755576aa18ac2bcaf8fab4da47d8d3f0baedd0
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2024-07-08 17:02:52 -0300

    Second Commit
    Second Commit

---
+++ mongoose/db/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/testemongoose");
+  console.log("Conectou ao MongoDB com o Mongoose!");
+}
+
+main().catch((err) => console.log(err));
+
+module.exports = mongoose;
